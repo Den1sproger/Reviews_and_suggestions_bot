e7b23a7447da846b87d3f7d4f0e8b58f88315ba6
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-08-07 14:58:42 +0300

    refactor: add logging and rename directory

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import logging
+
 from aiogram import executor
 from telegram_bot import dp
-from telegram_bot.handlers.general.start import *
-from telegram_bot.handlers.general.default_commands import *
+from telegram_bot.handlers.common.start import *
+from telegram_bot.handlers.common.default_commands import *
 from telegram_bot.handlers.admin_panel.reviews import *
 from telegram_bot.handlers.admin_panel.suggestions import *
+
+
+LOG_FILENAME = "/home/reviews_suggestions/py_log.log"
+logging.basicConfig(level=logging.INFO, filename=LOG_FILENAME, filemode="w")
 
 
 
